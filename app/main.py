e1e32cd7bb36c6b3496eb7c91071fe76feeeceb9
Ahtisham Ul Haq <f24607049@nutech.edu.pk>
2026-07-14 11:58:39 +0500

    Set allow_credentials to False in CORS middleware
    Change CORS middleware to disallow credentials.

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ app = FastAPI(title="ScanMyNotes API", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
